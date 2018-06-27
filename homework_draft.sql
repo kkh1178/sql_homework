e051f8b9a878f12422f9e3c5a1926c35104a3fb6
kkh1178 <kalisha.hill@gmail.com>
2018-06-27 12:02:22 -0500

    Answered more homework questions

--- homework_draft.sql
+++ homework_draft.sql
@@ -67,3 +67,85 @@ select last_name, count(last_name)
 from actor
 group by last_name
 having count(last_name)>=2;
+
+-- 4c. Oh, no! The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS, 
+-- the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
+
+select * from actor
+where last_name = 'WILLIAMS';
+
+UPDATE actor
+ SET first_name = 'HARPO', actor_name='HARPO WILLIAMS'
+ WHERE actor_id = 172;
+ 
+--  4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name 
+-- after all! In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO. Otherwise, 
+-- change the first name to MUCHO GROUCHO, as that is exactly what the actor will be with the grievous error. 
+-- BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO MUCHO GROUCHO, HOWEVER! 
+-- (Hint: update the record using a unique identifier.)
+
+-- I'm not sure what this is asking me to do.
+
+select * from actor
+where actor_name like '%HARPO%';
+
+UPDATE actor
+ SET first_name = 'GROUCHO', actor_name='GROUCHO WILLIAMS'
+ WHERE actor_id = 172;
+
+-- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
+-- Hint: https://dev.mysql.com/doc/refman/5.7/en/show-create-table.html
+SHOW create table address;
+
+-- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. 
+-- Use the tables staff and address:
+select * from staff;
+select * from address;
+
+SELECT staff.first_name, staff.last_name, address.address
+FROM staff
+INNER JOIN address ON staff.address_id=address.address_id;
+
+-- 
+-- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
+select * from payment;
+
+select staff_id, count(amount)
+from payment
+where payment_date like '2005%'
+group by staff_id;
+
+-- 
+-- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
+select * from film_actor;
+select * from film;
+
+select title from film;
+-- 
+-- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
+-- 
+-- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
+-- 
+-- 	![Total amount paid](Images/total_payment.png)
+-- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+-- 
+-- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
+-- 
+-- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. Use joins to retrieve this information.
+-- 
+-- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as famiy films.
+-- 
+-- 7e. Display the most frequently rented movies in descending order.
+-- 
+-- 7f. Write a query to display how much business, in dollars, each store brought in.
+-- 
+-- 7g. Write a query to display for each store its store ID, city, and country.
+-- 
+-- 7h. List the top five genres in gross revenue in descending order. (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+-- 
+-- 8a. In your new role as an executive, you would like to have an easy way of viewing the Top five genres by gross revenue. Use the solution from the problem above to create a view. If you haven't solved 7h, you can substitute another query to create a view.
+-- 
+-- 8b. How would you display the view that you created in 8a?
+-- 
+-- 8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
+-- 
