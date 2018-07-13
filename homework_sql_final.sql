6c253d82caa4503a787622c7670af93412dfa038
kkh1178 <kalisha.hill@gmail.com>
2018-07-13 17:41:26 -0500

    Removed the select alls that were annoying Jay :)

--- homework_sql_final.sql
+++ homework_sql_final.sql
@@ -4,7 +4,7 @@ Use sakila;
 Select first_name, last_name from actor;
 
 -- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
-select * from actor;
+-- select * from actor;
 
 Alter table actor
  add actor_name VARCHAR(50);
@@ -99,8 +99,8 @@ SHOW create table address;
 
 -- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. 
 -- Use the tables staff and address:
-select * from staff;
-select * from address;
+-- select * from staff;
+-- select * from address;
 
 SELECT staff.first_name, staff.last_name, address.address
 FROM staff
@@ -108,7 +108,7 @@ INNER JOIN address ON staff.address_id=address.address_id;
 
 -- 
 -- 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. Use tables staff and payment.
-select * from payment;
+-- select * from payment;
 
 select staff_id, count(amount)
 from payment
@@ -117,8 +117,8 @@ group by staff_id;
 
 -- 
 -- 6c. List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join.
-select * from film_actor;
-select * from film;
+-- select * from film_actor;
+-- select * from film;
 
 
 SELECT film.title, count(film_actor.actor_id) as 'Count_Actor'
@@ -127,8 +127,8 @@ INNER JOIN film ON film.film_id = film_actor.film_id
 group by film.title;
 -- 
 -- 6d. How many copies of the film Hunchback Impossible exist in the inventory system?
-select * from inventory;
-select * from film;
+-- select * from inventory;
+-- select * from film;
 
 SELECT film.title, count(inventory.film_id)
 FROM inventory
@@ -138,8 +138,8 @@ group by film.title;
 
 -- 
 -- 6e. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name:
-select * from payment;
-select * from customer;
+-- select * from payment;
+-- select * from customer;
 
 Select customer.last_name, sum(payment.amount)
 from payment
@@ -150,8 +150,8 @@ order by customer.last_name;
 
 -- 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. As an unintended consequence, films starting with the 
 -- letters K and Q have also soared in popularity. Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
-select * from film;
-select * from language;
+-- select * from film;
+-- select * from language;
 
 Select language.name, film.title 
 from language
@@ -166,9 +166,9 @@ AND language.name like 'English';
 
 -- 7b. Use subqueries to display all actors who appear in the film Alone Trip.
 
-select * from actor;
-select * from film;
-select * from film_actor;
+-- select * from actor;
+-- select * from film;
+-- select * from film_actor;
 
 Select actor_name
 from actor
@@ -186,10 +186,10 @@ where actor_id in (
 -- 
 -- 7c. You want to run an email marketing campaign in Canada, for which you will need the names and email addresses of all Canadian customers. 
 -- Use joins to retrieve this information.
-select * from customer;
-select * from address;
-select * from country;
-select * from city;
+-- select * from customer;
+-- select * from address;
+-- select * from country;
+-- select * from city;
 
 Select c.first_name, c.last_name, c.email, country.country 
 from customer as c
@@ -203,9 +203,9 @@ where country.country like 'Canada';
 -- 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. Identify all movies categorized as famiy films.
 -- 
 
-select * from film_category;
-select * from category;
-select * from film;
+-- select * from film_category;
+-- select * from category;
+-- select * from film;
 
 Select category.name, film.title
 from film_category
@@ -216,10 +216,10 @@ where category.name = 'Family';
 
 -- 7e. Display the most frequently rented movies in descending order.
 
-select * from film;
-select * from payment;
-select * from rental;
-select * from inventory;
+-- select * from film;
+-- select * from payment;
+-- select * from rental;
+-- select * from inventory;
 
 Select count(f.title), f.title
 from rental as r
@@ -232,8 +232,8 @@ order by 1 desc;
 -- 7f. Write a query to display how much business, in dollars, each store brought in.
 -- 
 
-select * from payment;
-select * from staff;
+-- select * from payment;
+-- select * from staff;
 
 select sum(p.amount), s.store_id 
 from payment as p
@@ -242,10 +242,10 @@ group by s.store_id;
 
 -- 7g. Write a query to display for each store its store ID, city, and country.
 
-select * from store;
-select * from address;
-select * from city;
-select * from country;
+-- select * from store;
+-- select * from address;
+-- select * from city;
+-- select * from country;
 
 select s.store_id, c.city, y.country
 from store as s
@@ -257,12 +257,12 @@ inner join country as y on c.country_id = y.country_id;
 -- payment, and rental.)
 -- 
 
-select * from category;
-select * from film_category;
-select * from inventory;
-select * from payment;
-select * from rental;
-
+-- select * from category;
+-- select * from film_category;
+-- select * from inventory;
+-- select * from payment;
+-- select * from rental;
+-- 
 select count(c.name), c.name, sum(p.amount)
 from payment as p
 inner join rental as r on r.rental_id = p.rental_id
